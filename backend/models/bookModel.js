88ad8f8e01b791866bb0f9f28e21fad8b0b13b43
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-07-03 16:47:28 -0700

    book and quote schema

---
+++ backend/models/bookModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    bookImg: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String
+    },
+    tags: {
+        type: String,
+    },
+
+    fav: {
+        type: Boolean,
+        default: true,
+    },
+    isFinishedReading: {
+        type: Boolean,
+        default: false,
+    },
+    quote: {
+        type: [Schema.Types.ObjectId],
+        ref: "Quote"
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book

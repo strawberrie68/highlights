cb6d57adeb813277d1b5be388b3c62ca0f6fcedf
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-07-03 16:57:18 -0700

    Book Detail -Create quote card component/ sidemenu

---
+++ real-highlights/src/components/CreateQuoteCard.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+export default function CreateQuoteCard() {
+    return (
+        <div>
+            <div className='createQuote-card m-3 px-4 border-2 border-dashed flex flex-col border-zinc-300'>
+                <div className='text-3xl text-zinc-300 my-2'>
+                    <FontAwesomeIcon icon="fa-solid fa-circle-plus" style={{ color: "#dbdbdb", }} />
+                </div>
+
+                <p className='px-6 text-zinc-300'>Add Quote</p>
+            </div>
+
+        </div>
+    )
+}

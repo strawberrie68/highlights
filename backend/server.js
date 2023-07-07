d36f853bbbd5dc7fce961e2b85976154a043f0b1
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-07-06 21:40:20 -0700

    inital files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose')
+const passport = require('passport');
 
 require("dotenv").config({ path: "./config/.env" });
 
+
+// routes
+
+const bookRouter = require('./routes/books')
+const quoteRouter = require('./routes/quotes')
+
+
+
+
 const app = express();
 const port = process.env.PORT || 6010;
+
+// pass the passport middleware
+app.use(passport.initialize());
 
 app.use(cors());
 app.use(express.json());
@@ -18,11 +31,12 @@ connection.once('open', () => {
     console.log("mongoDB database connection established successfully")
 })
 
-const bookRouter = require('./routes/books')
-const quoteRouter = require('./routes/quotes')
+
+//  routes
 
 app.use('/books', bookRouter);
 app.use('/quotes', quoteRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
